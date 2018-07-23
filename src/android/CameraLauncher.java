0800d5e13ce4333f2f4c8276dd88cdb9b9c66c0d
arhaha <zhuofang@celllink.io>
2018-07-23 17:29:08 +0800

    4.0.6

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -399,8 +399,9 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
             // croppedUri = Uri.fromFile(photo);
             // intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, croppedUri);
             // } else {
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            //intent.setAction(Intent.ACTION_GET_CONTENT);
+            //intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setAction(Intent.ACTION_PICK);
             // }
         } else if (this.mediaType == VIDEO) {
             intent.setType("video/*");
